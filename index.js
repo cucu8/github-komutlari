4577d5b3f019bd1787e66c46718435948575850d
cucu <cuneytdonmez8@gmail.com>
2021-12-20 19:06:19 +0300

    git ana komutlari

---
+++ index.js
@@ -0,0 +1,8 @@
+//selam index.js
+//ilk önce terminalden git status deriz. değişen dısyaların durumlarına bakarız.
+//git add .  diyerek bunları commit edilmeye hazırlarız.
+// git commit -m "gerekli aciklama satiri" ile local repomuza kaydederiz.
+//git push origin master ile de uzaktaki repomuza yapılan değişiklikleri kaydederiz. master branchinde calistigimiz icin git push origin master deriz. aslına hangi branchde calisiyorsak onu pısh ederiz.
+
+//git checkout -b yenibranch komutu ile yeni branch oluştururuz.
+//yeni branche geçildi.
