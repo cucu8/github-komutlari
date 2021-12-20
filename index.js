6fdb12280022c67e7c287b7c17c7dc69f7fbfd70
cucu <cuneytdonmez8@gmail.com>
2021-12-20 19:34:30 +0300

    ilgili eğitimin youtube linki eklendi

--- index.js
+++ index.js
@@ -6,3 +6,8 @@
 
 //git checkout -b yenibranch komutu ile yeni branch oluştururuz.
 //yeni branch değişikliği.
+
+//daha sonra git status , git add . , git commit -m "new brach değişiklikleri yapıldı" , git push origin yenibranch, diyerek brancimizi uzak repumuza ekleriz. asıl olan projemize dahil demek icin ise ; pullrequest deriz.  ordan newpullrequest deriz.
+//base olarak orjinal projemizi , compare olarak ise birleştirmek istedigimiz branchı seceriz.  daha sonra createpullrequest buyonuna basariz.  able to merge yazısı ile birleştirmiş oluruz.
+
+//https://www.youtube.com/watch?v=fnxi12JLhNY => ilgili eğitim videosunun linki 
